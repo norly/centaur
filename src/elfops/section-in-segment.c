c4d9a1aaf7c25e45adcaaebc970429f2c61bf6e9
norly <ny-git@enpas.org>
2013-02-23 15:47:53 +0000

    Merge {first,last}-section-in-segment.c

--- src/elfops/section-in-segment.c
+++ src/elfops/section-in-segment.c
@@ -8,6 +8,37 @@
 
 
 /*
+ * Returns the section that starts at the same point in the file as
+ * the segment AND is wholly contained in the memory image.
+ *
+ * If no section fits, NULL is returned.
+ */
+Elf_Scn* elfu_firstSectionInSegment(Elf *e, GElf_Phdr *phdr)
+{
+  Elf_Scn *scn;
+
+  scn = elf_getscn(e, 1);
+  while (scn) {
+    GElf_Shdr shdr;
+
+    if (gelf_getshdr(scn, &shdr) != &shdr) {
+      return NULL;
+    }
+
+    if (shdr.sh_offset == phdr->p_offset
+        && elfu_segmentContainsSection(phdr, &shdr)) {
+      return scn;
+    }
+
+    scn = elf_nextscn(e, scn);
+  }
+
+  return NULL;
+}
+
+
+
+/*
  * Returns the first section that  is contained in the segment and
  * ends as close to its memory image of as possible (the "last"
  * section in the segment).
